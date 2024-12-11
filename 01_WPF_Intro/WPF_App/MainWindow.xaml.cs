b74054a35e82bd551026fa987b5f4de1c92e50d4
Rajitha Udawalpola <rajitha@eie.ruh.ac.lk>
2024-12-11 10:39:47 +0530

    initial

---
+++ 01_WPF_Intro/WPF_App/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSayHelloButtonClick(object sender, RoutedEventArgs e)
+        {
+            string name = NameInput.Text;
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                GreetingText.Text = $"Hello, {name}!";
+            }
+            else
+            {
+                GreetingText.Text = "Please enter your name.";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
+        }
+    }
+}

6013c37294383db891d9451e4118f8a4f48b6e72
Rajitha Udawalpola <rajitha@eie.ruh.ac.lk>
2024-12-12 08:08:17 +0530

    wpf2

---
+++ 02-WpfApp-DataBinding/WpfApp/StringToBrushConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+public class StringToBrushConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string colorName)
+        {
+            try
+            {
+                return new SolidColorBrush((Color)ColorConverter.ConvertFromString(colorName));
+            }
+            catch
+            {
+                return Brushes.Black; // Default color if conversion fails
+            }
+        }
+        return Brushes.Black;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
